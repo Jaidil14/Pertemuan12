33d207ed2137d42e7357772efa58266f1cabe919
Jaidil14 <jaidilrachmad@gmail.com>
2025-12-27 09:03:53 +0700

    menghapus fungsi dan memanggil kelas uicontroller

--- app/src/main/java/com/example/pertemuan12/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan12/MainActivity.kt
@@ -7,41 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.pertemuan12.ui.theme.Pertemuan12Theme
+import com.example.pertemuan12.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Pertemuan12Theme {
+            Pertemuan12Theme() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Pertemuan12Theme {
-        Greeting("Android")
     }
 }
